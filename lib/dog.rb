391c32940f1a2d0f08a3afd0ea91511eb0abd34f
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-05 01:23:25 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,15 @@
-class Dog
+class Dog < ActiveRecord::Base
+  attr_accessor :name, :breed
+
+  def self.update(name:)
+
+  end
+
+  def self.find_or_create_by(name:,breed:)
+
+  end
+
+  def self.find_by_name(name)
+
+  end
 end
