4ca283ee23d4acd06cca781dfcf6e2dcb15dd6c2
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-05 01:43:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
 require 'pry'
 class Dog < ActiveRecord::Base
-  attr_accessor :name, :breed, :id
-
-  def update(name)
-    self.class
-
-    end
-    self
-  end
-
-  def self.find_or_create_by(name:,breed:)
-
-  end
+  # attr_accessor :name, :breed, :id
+  #
+  # def update(name)
+  #   self.class
+  #
+  #   end
+  #   self
+  # end
+  #
+  # def self.find_or_create_by(name:,breed:)
+  #
+  # end
 
 
 end
